51c8864a094fca6274f85e64d6fe005ea018cf1e
remarshal <marshalwaldendev@gmail.com>
2022-05-10 20:27:03 -0400

    initial setup

---
+++ components/Directory.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {FlatList} from 'react-native'
+import { ListItem } from 'react-native-elements';
+
+function Directory(props) {
+    return ()
+}
+
+export default Directory
