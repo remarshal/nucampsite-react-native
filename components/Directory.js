ecdee03841cf42d66b23048425cfce6efd8bdfc8
remarshal <marshalwaldendev@gmail.com>
2022-05-11 13:38:31 -0400

    updated ListItem

--- components/Directory.js
+++ components/Directory.js
@@ -8,6 +8,7 @@ function Directory(props) {
       <ListItem
         title={item.name}
         subtitle={item.description}
+        onPress={() => props.onPress(item.id)}
         leftAvatar={{ source: require("./images/react-lake.jpg") }}
       />
     );
