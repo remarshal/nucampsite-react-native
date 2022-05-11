7c7cf348c6e12fa7df8f9b82bad4ee1d15e319f1
remarshal <marshalwaldendev@gmail.com>
2022-05-11 13:27:34 -0400

    added Main component

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Main from "./components/Main";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My first react-native app!</Text>
+      <Main />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +16,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
