db5f9aa31788b155c595b0c9b1a379ff9c9adc0b
remarshal <marshalwaldendev@gmail.com>
2022-05-11 13:27:52 -0400

    updated Campsite to CAMPSITE

--- components/Main.js
+++ components/Main.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import Directory from "./Directory";
-import { Campsites } from "../shared/campsites";
+import { CAMPSITES } from "../shared/campsites";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      campsites: Campsites,
+      campsites: CAMPSITES,
     };
   }
 
