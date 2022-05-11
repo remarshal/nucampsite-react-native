ad970b31988b4daf85c2834d9869c1a1ead199c6
remarshal <marshalwaldendev@gmail.com>
2022-05-11 12:22:34 -0400

    initial setup

--- components/Directory.js
+++ components/Directory.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import {FlatList} from 'react-native'
-import { ListItem } from 'react-native-elements';
+import React from "react";
+import { FlatList } from "react-native";
+import { ListItem } from "react-native-elements";
 
 function Directory(props) {
-    return ()
+  const renderDirectoryItem = ({ item }) => {
+    return (
+      <ListItem
+        title={item.name}
+        subtitle={item.description}
+        leftAvatar={{ source: require("./images/react-lake.jpg") }}
+      />
+    );
+  };
+
+  return (
+    <FlatList
+      data={props.campsites}
+      renderItem={renderDirectoryItem}
+      keyExtractor={(item) => item.id.toString()}
+    />
+  );
 }
 
-export default Directory
+export default Directory;
