88d6288fdb94fe6a43481932aed9d78ac66706c2
remarshal <marshalwaldendev@gmail.com>
2022-05-14 11:25:18 -0400

    updated card info

--- components/Contact.js
+++ components/Contact.js
@@ -10,17 +10,13 @@ class Contact extends Component {
   render() {
     return (
       <ScrollView>
-        <RenderItem
-          item={this.state.campsites.filter((campsite) => campsite.featured)[0]}
-        />
-        <RenderItem
-          item={
-            this.state.promotions.filter((promotion) => promotion.featured)[0]
-          }
-        />
-        <RenderItem
-          item={this.state.partners.filter((partner) => partner.featured)[0]}
-        />
+        <Card style={{ margin: 20 }} title="Contact Information">
+          <Text>1 Nucamp Way</Text>
+          <Text>Seattle, WA 98001</Text>
+          <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+          <Text>Phone: 1-206-555-1234</Text>
+          <Text>Email: campsites@nucamp.co</Text>
+        </Card>
       </ScrollView>
     );
   }
