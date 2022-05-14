6e14a8a7a74ea97be4cb26b8d081ecca5e3489e0
remarshal <marshalwaldendev@gmail.com>
2022-05-14 12:01:42 -0400

    added mission and renderPartner

--- components/About.js
+++ components/About.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
-import { View, Text, ScrollView } from "react-native";
+import { Text, ScrollView } from "react-native";
 import { Card } from "react-native-elements";
+import { FlatList } from "react-native-gesture-handler";
+import { ListItem } from "react-native-elements";
+import { PARTNERS } from "../shared/partners";
 
 function Mission() {
   return (
@@ -18,14 +21,38 @@ function Mission() {
   );
 }
 class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      partners: PARTNERS,
+    };
+  }
+
   static navigationOptions = {
     title: "About Us",
   };
 
   render() {
+    const renderPartner = ({ item }) => {
+      return (
+        <ListItem
+          title={item.name}
+          subtitle={item.description}
+          leftAvatar={{ source: require("./images/bootstrap-logo.png") }}
+        />
+      );
+    };
+
     return (
       <ScrollView>
         <Mission />
+        <Card title="Community Partners">
+          <FlatList
+            date={this.state.partners}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={renderPartner}
+          />
+        </Card>
       </ScrollView>
     );
   }
