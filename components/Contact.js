75a0750d4f2faeb5db9224f1994c1d8d94cd537d
remarshal <marshalwaldendev@gmail.com>
2022-05-14 11:37:56 -0400

    removed View import

--- components/Contact.js
+++ components/Contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, ScrollView } from "react-native";
+import { Text, ScrollView } from "react-native";
 import { Card } from "react-native-elements";
 
 class Contact extends Component {
