03ca3ad998fbef81fd3f90bf7ae368a30da75da7
remarshal <marshalwaldendev@gmail.com>
2022-05-14 10:50:58 -0400

    initial setup

---
+++ components/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { View, Text, ScrollView } from "react-native";
+import { Card } from "react-native-elements";
+
+class About extends Component {
+  static navigationOptions = {
+    title: "About Us",
+  };
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default About;
---
+++ components/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { View, Text, ScrollView } from "react-native";
+import { Card } from "react-native-elements";
+
+class Contact extends Component {
+  static navigationOptions = {
+    title: "Contact Us",
+  };
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Contact;
