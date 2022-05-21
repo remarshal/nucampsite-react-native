2aa4fdae18964424041cfbf3e010c2f047f409ce
remarshal <marshalwaldendev@gmail.com>
2022-05-21 13:11:07 -0400

    updated comment id

--- redux/comments.js
+++ redux/comments.js
@@ -10,6 +10,7 @@ export const comments = (state = { errMess: null, comments: [] }, action) => {
 
     case ActionTypes.ADD_COMMENT:
       const comment = action.payload;
+      comment.id = state.comments.length;
       return { ...state, comments: state.comments.concat(comment) };
 
     default:
