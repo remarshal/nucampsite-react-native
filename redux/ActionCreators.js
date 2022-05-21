27e1c968c6703ce49aec1c2c83355c91bd1ee534
remarshal <marshalwaldendev@gmail.com>
2022-05-21 11:43:09 -0400

    added comment creator

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -181,7 +181,7 @@ export const postComment = (campsiteId, rating, author, text) => (dispatch) => {
   }, 2000);
 };
 
-export const addComment = (campsiteId) => ({
+export const addComment = (comment) => ({
   type: ActionTypes.ADD_COMMENT,
-  payload: campsiteId,
+  payload: comment,
 });
