bd91ef6d19623f2451a09c7202c8319b6d7e934c
remarshal <marshalwaldendev@gmail.com>
2022-05-21 11:37:10 -0400

    added post comment

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -165,3 +165,23 @@ export const addFavorite = (campsiteId) => ({
   type: ActionTypes.ADD_FAVORITE,
   payload: campsiteId,
 });
+
+export const postComment = (campsiteId, rating, author, text) => (dispatch) => {
+  const newComment = {
+    campsiteId,
+    rating,
+    author,
+    text,
+  };
+
+  newComment.date = new Date().toISOString();
+
+  setTimeout(() => {
+    dispatch(addComment(campsiteId));
+  }, 2000);
+};
+
+export const addComment = (campsiteId) => ({
+  type: ActionTypes.ADD_COMMENT,
+  payload: campsiteId,
+});
